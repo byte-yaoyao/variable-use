ae0b752e8b169481a4629600524f5959a79db47c
byte-yaoyao <2282580415@qq.com>
2020-12-24 15:50:41 +0800

    数据类型
    整形提升
    
    数值转换

---
+++ VariableUse/src/VariableUse.java
@@ -0,0 +1,36 @@
+public class VariableUse {
+    public static void main(String[] args) {
+//        int w = 7;
+//        int e = 2;
+//        System.out.println(w * e);
+//        w = 5;
+//        e = 3;
+//        System.out.println(w * e);
+//        int a = 10;
+//        long b = 20;
+//        //a = b;//编译出错，提示可能会损失精度
+//        b = a;//编译通过
+//        System.out.println(b);
+//        int c = 30;
+//        double d = 40;
+//        //c = d;//编译出错，提示可能会损失精度
+//        d = c;//编译通过
+//        System.out.println(d);
+//        int a = 10;
+//        double b = 20.5;
+//        a = (int)b;
+//        System.out.println(a);
+
+
+//        int a = 10;
+//        long b = 20;
+//        long d = a + b;
+//        System.out.println(d);
+
+//
+
+
+//
+
+    }
+}
